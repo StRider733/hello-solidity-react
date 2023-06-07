3cb8c42e383106b2485d5aa3a1b4b4a6f22be673
StRider733 <fhmrider@gmail.com>
2023-06-08 01:01:42 +0300

    first commit

---
+++ first-dapp/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import {ethers} from 'ethers';
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-Header">
+        <button>Get Greeting </button>
+        <button>Set Greeting </button>
+        <input />
+      </header>
+    </div>
+    </>
+  );
+}
+
+export default App;
