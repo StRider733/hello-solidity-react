0b001c4dc6b27f729b7d2d5234da022720092efe
StRider733 <fhmrider@gmail.com>
2023-06-10 01:32:55 +0300

    greet update

--- first-dapp/src/App.js
+++ first-dapp/src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
 import {ethers} from 'ethers';
+import GreetingContract from "./abis/HelloSolidity.json";
 
-
+const greetingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 function App() {
+
+  console.log(window);
+  async function requestAccount(){
+    await window.ethereum.request({method: "eth_requestAccount"});
+  }
+ requestAccount();
+
+  async function getGreeting(){
+    if(window.ethereum !== "undefined"){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greetingAddress,GreetingContract,provider);
+      
+      try {
+        const greeting = await contract.greet();
+        console.log(greeting);
+        
+      } catch (err) {
+        console.log(err);
+      }
+      
+      
+      console.log(contract);
+        
+    }
+  }
+  getGreeting();
+
+
+  async function setGreeting(){
+
+  }
+
+
   return (
     <>
     <div className="App">
       <header className="App-Header">
-        <button>Get Greeting </button>
+        <button onClick={getGreeting}>Get Greeting </button>
         <button>Set Greeting </button>
         <input />
       </header>
