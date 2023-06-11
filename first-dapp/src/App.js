61091f80cf06650ccc2416b5abc85c2b124beabf
StRider733 <fhmrider@gmail.com>
2023-06-11 14:27:12 +0300

    button configuration

--- first-dapp/src/App.js
+++ first-dapp/src/App.js
@@ -1,24 +1,39 @@
 import './App.css';
 import {ethers} from 'ethers';
 import GreetingContract from "./abis/HelloSolidity.json";
+import React, { useState, useEffect } from 'react';
 
-const greetingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const greetingAddress = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
+
 function App() {
 
-  console.log(window);
-  async function requestAccount(){
-    await window.ethereum.request({method: "eth_requestAccount"})
-  }
- requestAccount();
+  const [greet,setGreet] = useState("");
+  const [newGreet,setNewGreet] = useState("");
+  
+  useEffect(()=>{
+    getGreeting();
+  },[]);
 
+  async function requestAccount(){
+    await window.ethereum.request({method: "eth_requestAccounts"})
+
+  }
+ 
   async function getGreeting(){
-    if(window.ethereum !== "undefined"){
+
+    
+
+    if(typeof window.ethereum !== "undefined"){
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(greetingAddress,GreetingContract,provider);
       
       try {
         const greeting = await contract.greet();
-        console.log(greeting);
+
+        setGreet(greeting);
+
+
+        console.log("Greeting: ", greeting);
         
       } catch (err) {
         console.log(err);
@@ -29,10 +44,31 @@ function App() {
         
     }
   }
-  getGreeting();
+  //getGreeting();
 
 
   async function setGreeting(){
+    if(typeof window.ethereum !== "undefined"){
+     await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      const contract = new ethers.Contract(greetingAddress,GreetingContract,provider.getSigner());
+
+      const transaction = await contract.setGreeting(newGreet);
+
+      await transaction.wait();
+
+      setGreet(newGreet);
+      setNewGreet("");
+
+
+
+
+
+
+    }
+    
 
   }
 
@@ -41,9 +77,10 @@ function App() {
     <>
     <div className="App">
       <header className="App-Header">
+        <h1>{greet}</h1>
         <button onClick={getGreeting}>Get Greeting </button>
-        <button>Set Greeting </button>
-        <input />
+        <button onClick={setGreeting}>Set Greeting </button>
+        <input type="text" value={newGreet} onChange={(e)=> setNewGreet(e.target.value)}  />
       </header>
     </div>
     </>
